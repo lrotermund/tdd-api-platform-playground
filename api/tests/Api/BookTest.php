df8c2bbca63b7e7e1a7e9f7d83e46edd6a52a21f
Lukas Rotermund <lukas.rotermund@live.de>
2022-04-03 15:49:15 +0200

    removed review to test the basic stuff first

--- api/tests/Api/BookTest.php
+++ api/tests/Api/BookTest.php
@@ -14,10 +14,6 @@ class BookTest extends ApiTestCase
             'description' => 'Clean code that works--now. This is the seeming contradiction that lies behind much of the pain of programming. Test-driven development replies to this contradiction with a paradox--test the program before you write it.',
             'author' => 'Kent Beck',
             'publicationDate' => '8. November 2002',
-            'reviews' => [
-                'The book is well written and the quality of the printed version is top notch but even if that wasn\'t the case I still would rate this 5 stars due to the content alone. TDD is a methodology that it is well worth the try even if at the end of the day might not fit your use case, also this book helps me realize how most of the explanations regarding the emergent design in TDD is wrong and how important is the refactoring step in the red-green-refactor cycle.',
-                'Exactly as described',
-            ],
         ]]);
 
         $this->assertResponseStatusCodeSame(201);
@@ -29,10 +25,6 @@ class BookTest extends ApiTestCase
             'description' => 'Clean code that works--now. This is the seeming contradiction that lies behind much of the pain of programming. Test-driven development replies to this contradiction with a paradox--test the program before you write it.',
             'author' => 'Kent Beck',
             'publicationDate' => '8. November 2002',
-            'reviews' => [
-                'The book is well written and the quality of the printed version is top notch but even if that wasn\'t the case I still would rate this 5 stars due to the content alone. TDD is a methodology that it is well worth the try even if at the end of the day might not fit your use case, also this book helps me realize how most of the explanations regarding the emergent design in TDD is wrong and how important is the refactoring step in the red-green-refactor cycle.',
-                'Exactly as described',
-            ],
         ]);
     }
 }
