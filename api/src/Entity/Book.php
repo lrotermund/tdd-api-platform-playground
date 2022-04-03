bc079998f55c7b1a5df8a7bad0be40d60066460c
Lukas Rotermund <lukas.rotermund@live.de>
2022-04-03 16:13:48 +0200

    test green: added ORM to the entites and added a test database

--- api/src/Entity/Book.php
+++ api/src/Entity/Book.php
@@ -5,30 +5,39 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
 
 /** A book. */
+#[ORM\Entity]
 #[ApiResource]
 class Book
 {
     /** The id of this book. */
+    #[ORM\Id, ORM\Column, ORM\GeneratedValue]
     private ?int $id = null;
 
     /** The ISBN of this book (or null if doesn't have one). */
+    #[ORM\Column(nullable: true)]
     public ?string $isbn = null;
 
     /** The title of this book. */
+    #[ORM\Column]
     public string $title = '';
 
     /** The description of this book. */
+    #[ORM\Column(type: "text")]
     public string $description = '';
 
     /** The author of this book. */
+    #[ORM\Column]
     public string $author = '';
 
     /** The publication date of this book. */
+    #[ORM\Column(type: "datetime")]
     public ?\DateTimeInterface $publicationDate = null;
 
     /** @var Review[] Available reviews for this book. */
+    #[ORM\OneToMany(mappedBy: "book", targetEntity: "Review", cascade: ["persist", "remove"])]
     public iterable $reviews;
 
     public function __construct()
--- api/src/Entity/Review.php
+++ api/src/Entity/Review.php
@@ -4,27 +4,35 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
 
 /** A review of a book. */
+#[ORM\Entity]
 #[ApiResource]
 class Review
 {
     /** The id of this review. */
+    #[ORM\Id, ORM\Column, ORM\GeneratedValue]
     private ?int $id = null;
 
     /** The rating of this review (between 0 and 5). */
+    #[ORM\Column(type: "smallint")]
     public int $rating = 0;
 
     /** The body of the review. */
+    #[ORM\Column(type: "text")]
     public string $body = '';
 
     /** The author of the review. */
+    #[ORM\Column]
     public string $author = '';
 
     /** The date of publication of this review.*/
+    #[ORM\Column(type: "datetime")]
     public ?\DateTimeInterface $publicationDate = null;
 
     /** The book this review is about. */
+    #[ORM\ManyToOne(targetEntity: 'Book', inversedBy: "reviews")]
     public ?Book $book = null;
 
     public function getId(): ?int
